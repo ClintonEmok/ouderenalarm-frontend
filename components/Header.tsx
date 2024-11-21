8f028c1c3fccf4c56752d3d22b7d069741807b27
Clinton <c.emok@student.tue.nl>
2024-11-21 16:59:46 +0100

    Blog header update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 interface HeaderProps {
   title: string;
+  topics?: boolean;
 }
-const Header = ({ title }: HeaderProps) => {
+const Header = ({ title, topics = false }: HeaderProps) => {
   return (
     <header className="py-14 px-4 mb-12 text-center border-b border-blue-700">
       <h2 className="uppercase text-2xl mx-auto max-w-2xl font-bold">
         {title}
       </h2>
+      {topics && (
+        <div className="text-xs mt-2">
+          <Link href="/blog/topic">#topics</Link>
+        </div>
+      )}
     </header>
   );
 };
