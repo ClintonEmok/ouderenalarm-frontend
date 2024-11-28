c74ff59d686f27d49c92859759433e83115dd925
Clinton <c.emok@student.tue.nl>
2024-11-28 21:37:20 +0100

    added background to bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="flexBetween max-container padding-contianer relative z-30 py-5 px-3">
+      <nav className="flexBetween max-container padding-contianer relative z-30 py-5 px-3 bg-blue-700">
         <Link href="/">
           {/* TODO: Change logo */}
           <Image
@@ -20,12 +20,12 @@ const NavBar = () => {
             alt="logo"
           />
         </Link>
-        <ul className="hidden h-full gap-12 lg:flex">
+        <ul className="hidden h-full gap-12  lg:flex">
           {NAV_LINKS.map((link) => (
             <Link
               href={link.href}
               key={link.key}
-              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+              className="regular-16 text-white flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
             >
               {link.label}
             </Link>
