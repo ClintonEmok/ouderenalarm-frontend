468a1c0c10a8a3cbe1b54df7a42fc7fe46e162b3
Clinton <c.emok@student.tue.nl>
2024-12-06 18:54:20 +0100

    Added placeholder icon to navbar

--- components/DashboardNavbar.tsx
+++ components/DashboardNavbar.tsx
@@ -1,9 +1,34 @@
+import { Search, Settings } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const DashboardNavbar = () => {
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3">
-      DashboardNavbar
+      {/* Searchbar */}
+      <div className="flex items-center gap-8">
+        <div className="relative flex h-min w-[200px]">
+          <Search
+            size={24}
+            className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer  "
+          />
+          <input
+            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:outline-none focus:border-transparent"
+            type="search"
+            placeholder="Search..."
+          />
+        </div>
+      </div>
+      {/* Icons */}
+      <div className="flex items-center  ">
+        <Link
+          href="/settings"
+          className="h-min w-mi rounded p-2 hover:bg-gray-100"
+        >
+          <Settings className="h-6 w-6 cursor-pointer" />
+        </Link>
+        <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-ray-200 md:inline-block"></div>
+      </div>
     </div>
   );
 };
